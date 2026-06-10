e324b2498c8aaac98d1cbd00ca490fae5ee2f486
aareia <ashton-77@hotmail.com>
2026-06-10 17:58:25 -0400

    Add Log.jsx, lift gameboard state and use derived gameboard state inside GameBoard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import { useState } from 'react';
 
 import Player from "./components/Player"
 import GameBoard from "./components/gameBoard"
+import Log from './components/Log';
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex){
     setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? "O" : "X");
+    setGameTurns(prevTurns => {
+      let currentPlayer ="X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player ==="X"){
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: {row: rowIndex, col: colIndex}, player: currentPlayer },
+        ...prevTurns];
+
+        return updatedTurns;
+    });
+
+    
   }
 
   return (
@@ -18,10 +35,13 @@ function App() {
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
 
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard 
+        onSelectSquare={handleSelectSquare} 
+        turns={gameTurns}
+        />
       </div>
 
-      LOG
+      <Log/>
     </main>
 
   )
