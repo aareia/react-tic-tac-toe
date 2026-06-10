7121bfe77baf06ab41d06b73391038c00d1f6f4f
aareia <ashton-77@hotmail.com>
2026-06-10 16:42:43 -0400

    Add edit player name functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Player from "./components/player"
 
 function App() {
   
 
   return (
-    <p>coming soon..</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbo="X" />
+          <Player initialName="Player 2" symbo="O" />
+        </ol>
+
+        GAME BOARD
+      </div>
+
+      LOG
+    </main>
 
   )
 }
