6cdd040a66d3818c4abbdf0400d849051a84b836
aareia <ashton-77@hotmail.com>
2026-06-11 14:26:05 -0400

    Remove ability for a button to be selected multiple times

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,7 +6,6 @@ const initialGameBoard = [
 
 export default function GameBoard({onSelectSquare, turns}){
     let gameBoard = initialGameBoard;
-    console.log(turns);
     for (const turn of turns){
         const {square, player} = turn;
         const {row, col} = square;
@@ -31,7 +30,12 @@ export default function GameBoard({onSelectSquare, turns}){
                 <ol>
                     {row.map((playerSymbol, colIndex) => 
                     <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        <button 
+                            onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                            disabled={playerSymbol !==null}
+                        >
+                            {playerSymbol}
+                        </button>
                     </li>
                     )}
                 </ol>
